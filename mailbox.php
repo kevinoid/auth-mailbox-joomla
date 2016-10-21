74955d9169032de53d380e4262e4014d7f1a88c0
Kevin Locke <kevin@kevinlocke.name>
2016-10-20 20:00:42 -0600

    Remove constructor
    The parameters have not been passed through the plugin constructor since
    joomla/joomla-cms@4aae80a7.  Move the loadLanguage call into
    onUserAuthenticate, as done in the GMail auth plugin.  Arguments to
    loadLanguage are no longer necessary.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- mailbox.php
+++ mailbox.php
@@ -29,24 +29,6 @@ jimport('joomla.plugin.plugin');
 class plgAuthenticationMailbox extends JPlugin
 {
 	/**
-	 * Constructor
-	 *
-	 * For php4 compatability we must not use the __constructor as a constructor for plugins
-	 * because func_get_args (void) returns a copy of all passed arguments NOT references.
-	 * This causes problems with cross-referencing necessary for the observer design pattern.
-	 *
-	 * @param	object	&$subject	The object to observe
-	 * @param	array	$config		An array that holds the plugin configuration
-	 *
-	 * @since	1.5
-	 */
-	function plgAuthenticationMailbox(& $subject, $config)
-	{
-		parent::__construct($subject, $config);
-		$this->loadLanguage('', JPATH_ADMINISTRATOR);
-	}
-
-	/**
 	 * Get the mailbox connection string for use in imap_open determined from
 	 * the current param values.
 	 *
@@ -116,6 +98,9 @@ class plgAuthenticationMailbox extends JPlugin
 	 */
 	function onUserAuthenticate($credentials, $options, &$response)
 	{
+		// Load translatable strings for this plugin
+		$this->loadLanguage();
+
 		$response->type = 'Mailbox';
 
 		if (!function_exists('imap_open')) {
