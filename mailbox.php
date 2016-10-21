041f9c40bbb557a8a7968a157d3710ddf3021f09
Kevin Locke <kevin@kevinlocke.name>
2016-10-20 19:42:04 -0600

    Support domain in username
    This commit adds support for including a domain in the login username
    and optionally in the resulting Joomla! username.  This fixes the
    usability issue noted in #12 that users may enter their full email
    address as their login username.
    
    It additionally adds support for (optionally) using the whole email
    address as the Joomla! username.  This can be necessary for use cases
    where the mail server hosts multiple domains with users allowed to
    access Joomla!.
    
    Fixes: #12
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- mailbox.php
+++ mailbox.php
@@ -131,28 +131,42 @@ class plgAuthenticationMailbox extends JPlugin
 			return;
 		}
 
+		$username = $credentials['username'];
+		$atpos = strpos($username, '@');
+
+		if ($atpos === false) {
+			$mailDomain = $this->params->get('mail_domain');
+
+			if ($mailDomain) {
+				$email = $username . '@' . $mailDomain;
+			}
+		} else {
+			$email = $username;
+			$username = substr($username, 0, $atpos);
+		}
+
+		$joomlaUsername =
+			isset($email) && $this->params->get('mail_domain_in_joomla_username') ?
+				$email :
+				$username;
+		$mailboxUsername =
+			isset($email) && $this->params->get('mail_domain_username') ?
+				$email :
+				$username;
+
 		// Check that the user exists, if required
 		if (!$this->params->get('create_users')) {
 			jimport('joomla.user.helper');
 
-			if (!JUserHelper::getUserId($credentials['username'])) {
+			if (!JUserHelper::getUserId($joomlaUsername)) {
 				$response->status = JAUTHENTICATE_STATUS_FAILURE;
 				$response->error_message = JText::_('JGLOBAL_AUTH_NO_USER');
 				return;
 			}
 		}
 
-		$mailboxStr = $this->_getMailboxString();
-		$username = $credentials['username'];
-		$domain = $this->params->get('mail_domain');
-		if ($domain) {
-			$email = $username . '@' . $domain;
-			if ($this->params->get('mail_domain_username')) {
-				$username = $email;
-			}
-		}
-
 		// Build mailbox options for imap_open
+		$mailboxStr = $this->_getMailboxString();
 		$mailboxOpts = 0;
 		$protocol = $this->params->get('mail_protocol');
 
@@ -179,13 +193,18 @@ class plgAuthenticationMailbox extends JPlugin
 		imap_errors();
 
 		JLog::add(
-			JText::sprintf('LOGIMAPOPEN', $mailboxStr, $username, $mailboxOpts),
+			JText::sprintf(
+				'LOGIMAPOPEN',
+				$mailboxStr,
+				$mailboxUsername,
+				$mailboxOpts
+			),
 			JLog::DEBUG
 		);
 
 		$mailboxStream = @imap_open(
 			$mailboxStr,
-			$username, $credentials['password'],
+			$mailboxUsername, $credentials['password'],
 			$mailboxOpts, 1
 		);
 
@@ -212,12 +231,15 @@ class plgAuthenticationMailbox extends JPlugin
 		imap_close($mailboxStream);
 
 		JLog::add(
-			JText::sprintf('LOGAUTHENTICATED', $credentials['username']),
+			JText::sprintf('LOGAUTHENTICATED', $joomlaUsername),
 			JLog::DEBUG
 		);
 
 		$response->status = JAUTHENTICATE_STATUS_SUCCESS;
 		$response->error_message = '';
+		$response->fullname = $joomlaUsername;
+		$response->username = $joomlaUsername;
+
 		if (isset($email)) {
 			$response->email = $email;
 		}
